2191608d701fb1c2e702bfad9fe8eaaef6c07881
unknown <adat.nutech@gmail.com>
2026-08-19 13:36:22 +0700

    update icon

--- src/components/ui/Icons.tsx
+++ src/components/ui/Icons.tsx
@@ -219,7 +219,6 @@ export default function Icons({
 
     return (
         <svg
-            xmlns="http://w3.org"
             viewBox="0 0 24 24"
             strokeWidth={2}
             width={size}
